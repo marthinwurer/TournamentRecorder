c17f7eff6b2e912425a80e0debe0ee269cd2c67f
Benjamin Maitland <marthinwurer@gmail.com>
2016-10-26 21:26:14 -0400

    Working on the db api

--- src/db_api.py
+++ src/db_api.py
@@ -27,16 +27,33 @@ def createPlayer(DCI, name):
     return '{"outcome":true}'
 
 def createTournament(name, max_rounds):
-    pass
+    curs = db.cursor()
+    curs.execute("""INSERT INTO tournament
+                        (name, max_rounds) VALUES
+                        (%s  , %s  ); """, [name, max_rounds])
+    db.commit()
+    return '{"outcome":true}'
 
 def finishRound(r_id):
-    pass
+    curs = db.cursor()
+    curs.execute("""UPDATE round
+                        SET end_date=NOW()
+                        WHERE id=%s; """, [r_id])
+    db.commit()
+    return '{"outcome":true}'
 
 def generatePairings(t_id):
     pass
 
 def getPlayer(p_id):
-    pass
+    curs = db.cursor()
+    curs.execute("""SELECT id, name FROM player
+                        WHERE id=%s; """, [p_id])
+    db.commit()
+    
+    result = curs.fetchone()
+    output = {'id': result[0], 'name': result[1]}
+    return output
 
 def listPlayers():
     curs = db.cursor()
@@ -51,13 +68,45 @@ def listPlayers():
     
 
 def listTournaments(sort_on, filter_types):
-    pass
+    curs = db.cursor()
+    curs.execute("""SELECT t.id, t.name, (
+                        SELECT COUNT(*) 
+                        FROM tournament_player as p
+                        WHERE p.t_id=t.id)
+                            as num_players
+                        FROM tournament as t; """, [])
+    db.commit()
+    
+    output = []
+
+    for row in curs.fetchall():
+        output.append({'id': row[0], 'name': row[1], 'num_players': row[2]})
+    return json.dumps(output)
 
 def listTournamentPlayers(t_id):
-    pass
+    curs = db.cursor()
+    curs.execute("""SELECT tp.id, tp.p_id, (
+                            SELECT name FROM player AS p WHERE p.id=tp.p_id
+                            ) AS name
+                        FROM tournament_player AS tp
+                        WHERE pt.t_id=%s; """, [t_id])
+    db.commit()
+    
+    result = curs.fetchone()
+    output = {'id': result[0], 'p_id': result[1], 'name': result[2]}
+    return output
 
 def matchList(r_id):
-    pass
+    curs = db.cursor()
+    # TODO
+    curs.execute("""SELECT id, p1 FROM player; """, [])
+    db.commit()
+    
+    output = []
+
+    for row in curs.fetchall():
+        output.append({'id': row[0], 'name': row[1]})
+    return json.dumps(output)
 
 def removePlayer(p_id, t_id):
     pass
