fd08cd8c2979d91049c6faf6584599bb4c7f5478
Jonathan Lo <jonathan.c.lo.us@gmail.com>
2016-11-11 20:19:22 -0500

    modulized menus

--- src/clientGUI.py
+++ src/clientGUI.py
@@ -24,24 +24,34 @@ class clientApp ( Tk ) :
 
     def action_createMenu ( self ) :
         global g_menubar
+        self.action_createMenu_file ( )
+        self.action_createMenu_tourn ( )
+        self.action_createMenu_players ( )
+
+    def action_createMenu_file ( self ) :
+        global g_menubar
 
         self.menu_file = Menu ( self.g_menubar )
         self.menu_file.add_command ( label = "Exit", command = self.g_master.quit )
 
+        self.g_menubar.add_cascade ( label = "File", menu = self.menu_file )
+
+    def action_createMenu_tourn ( self ):
         self.menu_tourn = Menu ( self.g_menubar )
         self.menu_tourn.add_command ( label = "List Tournament", command = self.action_listTournaments )
         self.menu_tourn.add_separator ()
         self.menu_tourn.add_command ( label = "Create Tournament", command = self.action_createTournament )
         self.menu_tourn.add_command ( label = "Start Tournament", command = self.action_startTournament )
 
+        self.g_menubar.add_cascade ( label = "Tournaments", menu = self.menu_tourn )
+
+    def action_createMenu_players ( self ) :
         self.menu_players = Menu ( self.g_menubar )
         self.menu_players.add_command ( label = "List Player", command = self.action_listPlayers )
         self.menu_players.add_separator ()
         self.menu_players.add_command ( label = "Create Player", command = self.action_createPlayer )
         self.menu_players.add_command ( label = "Start Player", command = self.action_addPlayer )
 
-        self.g_menubar.add_cascade ( label = "File", menu = self.menu_file )
-        self.g_menubar.add_cascade ( label = "Tournaments", menu = self.menu_tourn )
         self.g_menubar.add_cascade ( label = "Players", menu = self.menu_players )
 
     def action_listTournaments ( self ) :
