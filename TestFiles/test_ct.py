53be35b8ac6f3d806885cf6cc9cef8864517411a
red1717 <evanditto9@gmail.com>
2016-11-18 16:48:31 -0500

    test_db_empty.py is finished

---
+++ TestFiles/test_ct.py
@@ -0,0 +1,17 @@
+"""
+This file tests the create tournament (ct) method of the API
+Author: TangentTally
+"""
+import unittest
+import sys
+sys.path.insert(0, "../src")
+import tr_api
+
+class Test_Ct(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
--- TestFiles/test_db_empty.py
+++ TestFiles/test_db_empty.py
@@ -12,12 +12,12 @@ class Test_Db_Empty(unittest.TestCase):
     def setUp(self):
         self.topdict = tr_api.listPlayers()
         self.topdict2 = tr_api.listTournaments(None, None)
-        self.topdict3 = tr_api.listTournamentPlayers()
-        tr_api.
 
     def tearDown(self):
         pass
 
     def test_no_players(self):
+        self.assertEqual(len(self.topdict.get('rows')), 0)
 
     def test_no_tournaments(self):
+        self.assertEqual(len(self.topdict2.get('rows')), 0)
