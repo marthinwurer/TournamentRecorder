1b17c11c4bb098b067a7a8dda445de6276097df7
Benjamin Maitland <marthinwurer@gmail.com>
2016-11-23 10:40:08 -0500

    Changed removePlayer to take the tp_id, instead of the p_id

--- src/tr_api.py
+++ src/tr_api.py
@@ -111,7 +111,7 @@ def matchList(r_id):
     """
     return sendRequest('matchList', {'r_id': r_id})
 
-def removePlayer(p_id, t_id):
+def removePlayer(tp_id, t_id):
     """
         Only do this if the player has no outstanding match results.
 
@@ -120,7 +120,7 @@ def removePlayer(p_id, t_id):
 
         :returns outcome
     """
-    return sendRequest('removePlayer', {'p_id': p_id, 't_id': t_id})
+    return sendRequest('removePlayer', {'p_id': tp_id, 't_id': t_id})
 
 def roundList(t_id):
     """
